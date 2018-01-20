abf12bd9bced03b5ebbe4e5a131243bcf225ee83
franciscothebrady <franciscothebrady@gmail.com>
2018-01-19 20:07:47 -0500

    isolaate top events and visualize.

---
+++ 5-top_events.R
@@ -0,0 +1,31 @@
+# francisco
+# 5. isolating some relationships
+# in this script i try to isolate some big effects so we can see if a relationship is present 
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(scales)
+
+# read in  the events data.
+events <- read.csv("data/4_sheldus.csv", stringsAsFactors = FALSE)
+# read in unemp separately 
+unemp <- read.csv("data/bls_vars.csv", stringsAsFactors = FALSE)
+# arrange by most populous and biggest damage impact
+top_events <- events %>% arrange(-pop_est, -adj.dmg.pcapita) %>%
+  head(20)
+
+# find unique counties in top events
+top_series <- unique(top_events$series.id)
+# filter unemployment to just those counties
+top_unemp <- unemp %>% filter(series.id %in% top_series) %>% arrange(date)
+
+top_dates <- top_events %>% select(series.id, EVENTS.begin_date) %>% 
+  arrange(EVENTS.begin_date) %>% distinct()
+
+# plot unemployment
+##
+# not sure why the event lines are not showing up.
+ggplot(data = top_unemp, aes(x =  date, y = unemp, group = series.id, color = series.id)) + geom_line() + 
+  geom_vline(xintercept = c(as.numeric(floor_date(ymd(EVENTS.begin_date), unit = "month")))), data = top_dates)
+             
+             
