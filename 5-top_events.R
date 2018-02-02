02ac6a038ef45133593b287bc059e2e7f63d892a
franciscothebrady <franciscothebrady@gmail.com>
2018-02-02 17:18:45 -0500

    new plots!

--- 5-top_events.R
+++ 5-top_events.R
@@ -42,11 +42,20 @@ unemplot <- ggplot(data = allthestuff, aes(x =  date, y = unemp, group = fcc.cou
   scale_x_date(date_breaks = waiver(), date_labels = "%Y-%b")
 
 unemplot
-  
+
+smaller_sample <- filter(allthestuff, fcc.county.name==c("clark","maricopa","riverside"))
+small_plot <- ggplot(data = smaller_sample, aes(x =  date, y = unemp, group = fcc.county.name)) + 
+  geom_line(aes(color = fcc.county.name)) + guides("FALSE") +
+  geom_point(data = smaller_sample, aes(x = EVENTS.begin_date, y = adj.dmg.pcapita,
+                                     group = fcc.county.name, color = fcc.county.name, size = adj.dmg.pcapita)) +
+  geom_vline(data = smaller_sample, aes(xintercept = as.numeric(EVENTS.begin_date),
+                                     color = factor(fcc.county.name)), show.legend = FALSE) +
+  scale_x_date(date_breaks = "3 months", date_labels = "%m-%y") + theme(axis.text.x = element_text(angle = -45, hjust = 0.5))
+small_plot
 # plot for event lines 
 # not sure why the event lines are not showing up.
-ggplot(data = top_unemp, aes(x =  date, y = unemp, group = fcc.county.name))  + geom_line(aes(color = fcc.county.name)) + 
-  geom_vline(data = top_dates, aes(xintercept = c(as.numeric(floor_date(ymd(EVENTS.begin_date), unit = "month")))))
+# ggplot(data = top_unemp, aes(x =  date, y = unemp, group = fcc.county.name))  + geom_line(aes(color = fcc.county.name)) + 
+#   geom_vline(data = top_dates, aes(xintercept = c(as.numeric(floor_date(ymd(EVENTS.begin_date), unit = "month")))))
 
 # ggplot(data = top_unemp, aes(x =  date, y = unemp, group = fcc.county.name)) + geom_line(aes(color = fcc.county.name)) +
 #   geom_point(data = top_dmg, aes(x = EVENTS.begin_date, y = adj.dmg.pcapita, 
@@ -55,11 +64,11 @@ ggplot(data = top_unemp, aes(x =  date, y = unemp, group = fcc.county.name))  + 
 
 
 # doesn't work
-cook.events <- top_unemp %>% filter(fcc.county.name=="cook") %>% 
-    left_join(filter(top_dates, series.id=="LAUCN170310000000003"), by = c("series.id","EVENTS.begin_date"))
-str(top_unemp$date)
-top_dates$series.id
-str(head(top_dates$EVENTS.begin_date))
+# cook.events <- top_unemp %>% filter(fcc.county.name=="cook") %>% 
+#     left_join(filter(top_dates, series.id=="LAUCN170310000000003"), by = c("series.id","EVENTS.begin_date"))
+# str(top_unemp$date)
+# top_dates$series.id
+# str(head(top_dates$EVENTS.begin_date))
 
 # plot dates on top of unemployment
-event_plot <- u + geom_vline(mapping = top_dates)
+# event_plot <- u + geom_vline(mapping = top_dates)
