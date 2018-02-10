21ebdee5d894a65f17dfad04fa77c84b515340a5
franciscothebrady <franciscothebrady@gmail.com>
2018-02-09 22:58:41 -0500

    figured it out. now need to filter for the more important series to only adjust them.

--- unemp_seasonal_adj.R
+++ unemp_seasonal_adj.R
@@ -1,17 +1,26 @@
-# seasonal adjustment of unemployment
-library(ggplot2)
-library(lubridate)
-library(scales)
+library(readr)
 library(tidyverse)
+library(reshape2)
+library(lubridate)
+bls_vars <- read_csv("/href/research7/m1fmb02/rstuff/bls_vars.csv")
+# extract start date
+start_date <- min(bls_vars$date)
+# convert to wide format
+bls_wide <- dcast(bls_vars, date ~ series.id, value.var = "unemp")
+# sanity check
+table(is.na(bls_wide))
+# turn into ts object 
+unemp.ts <- ts(bls_wide[,-1], start = c(2010,1), frequency = 12) # this didn't like start = start_date for some reason.
+# seasonally adjust!
+library(seasonal)
+# this works but on all 1400 counties. too much!
+unemp.seas <- seas(unemp.ts)
+plot(unemp.seas)
+#  you can subset by columns
+unemp_small <- seas(unemp.ts[, c(1:10)])
+plot(unemp_small)
+# the trick is to subset the df before it's turned into a seas object
+seas(unemp.ts[,c(grep(unemp.ts,'LAUCN0401300000000'))]) # doesn't work
 
-# read in unemployment data
-unemp <- read.csv("data/bls_vars.csv", stringsAsFactors = FALSE)
-
-# change dates to date class
-unemp$date <- as.Date(unemp$date)
-
-# turn into ts object by series
-names(unemp)
-# first spread data so there's only one date column
-unemp.ts <- spread(data = unemp, key = series.id, value = unemp, drop = TRUE)
-
+maricopa.adj <- seas(ts(bls_wide$LAUCN040130000000003, start = c(2010,1), frequency = 12)) # works
+plot(maricopa.adj)
