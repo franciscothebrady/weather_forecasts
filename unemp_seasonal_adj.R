e8b5ef2b19f09275393166f402e1cac7dd37b2bc
Francisco <franciscothebrady@gmail.com>
2018-03-07 15:29:39 -0500

    experiments with ts and lappy

--- unemp_seasonal_adj.R
+++ unemp_seasonal_adj.R
@@ -10,36 +10,44 @@ bls_vars <- read_csv("data/bls_vars.csv")
 start_date <- min(bls_vars$date)
 # convert to wide format
 bls_wide <- dcast(bls_vars, date ~ series.id, value.var = "unemp")
-str(bls_wide)
-# sanity check
-table(is.na(bls_wide))
-# turn into ts object 
-unemp.ts <- ts(bls_wide[,-1], start = c(2010,1), frequency = 12) # this didn't like start = start_date for some reason.
-# seasonally adjust!
-# install.packages("seasonal")
+# create a list of dataframes split on the series IDs
+bls_split <- split.data.frame(bls_vars, f = bls_vars$series.id)
+
+# apply seasonal adjustment to each list element
 library(seasonal)
-# this works but on all ~1400 counties. too much!
-unemp.seas <- seas(unemp.ts)
-plot(unemp.seas)
+# first turn the list into ts objects
+unemp_ts <- lapply(bls_split, ts(x, start = c(2010,1), frequency = 12))
 
-# the gsdc package looks promising to convert the ts object back into a df for ggplot2 style visualizations
-# https://cran.r-project.org/web/packages/ggseas/vignettes/ggsdc.html
-rm(unemp.ts)
-#  you can subset by columns
-unemp_small <- seas(unemp.ts[, c(1:10)])
-plot(unemp_small)
-# the trick is to subset the df before it's turned into a seas object
-seas(unemp.ts[,c(grep(unemp.ts,'LAUCN0401300000000'))]) # doesn't work
-seas(ts(bls_wide$LAUCN040010000000003, start = c(2010,1), fre
-maricopa.adj <- seas(ts(bls_wide$LAUCN040130000000003, start = c(2010,1), frequency = 12)) # works
-plot(maricopa.adj)
-
-# convert back into dataframe for plotting. 
-# save series names, which can be matched up with counties later
-series_names <- unemp.seas$x
-names(as.data.frame(unemp.seas))
-
-series_we_want <- unique(events$series.id))
-
-unemp_we_Want <- bls_wide %>% select(series_we_want)) # 
-split_test <- split(unemp_we_Want, f = unemp_we_Want$)
+#### OLD STUFF ####
+# # sanity check
+# table(is.na(bls_wide))
+# # turn into ts object 
+# unemp.ts <- ts(bls_wide[,-1], start = c(2010,1), frequency = 12) # this didn't like start = start_date for some reason.
+# # seasonally adjust!
+# # install.packages("seasonal")
+# library(seasonal)
+# # this works but on all ~1400 counties. too much!
+# unemp.seas <- seas(unemp.ts)
+# plot(unemp.seas)
+# 
+# # the gsdc package looks promising to convert the ts object back into a df for ggplot2 style visualizations
+# # https://cran.r-project.org/web/packages/ggseas/vignettes/ggsdc.html
+# rm(unemp.ts)
+# #  you can subset by columns
+# unemp_small <- seas(unemp.ts[, c(1:10)])
+# plot(unemp_small)
+# # the trick is to subset the df before it's turned into a seas object
+# seas(unemp.ts[,c(grep(unemp.ts,'LAUCN0401300000000'))]) # doesn't work
+# seas(ts(bls_wide$LAUCN040010000000003, start = c(2010,1), fre
+# maricopa.adj <- seas(ts(bls_wide$LAUCN040130000000003, start = c(2010,1), frequency = 12)) # works
+# plot(maricopa.adj)
+# 
+# # convert back into dataframe for plotting. 
+# # save series names, which can be matched up with counties later
+# series_names <- unemp.seas$x
+# names(as.data.frame(unemp.seas))
+# 
+# series_we_want <- unique(events$series.id))
+# 
+# unemp_we_Want <- bls_wide %>% select(series_we_want)) # 
+# split_test <- split(unemp_we_Want, f = unemp_we_Want$)
