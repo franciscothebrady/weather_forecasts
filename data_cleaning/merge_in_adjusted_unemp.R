d59e3cf0da365c4ff5270f091e42d54d7d23144d
Francisco <franciscothebrady@gmail.com>
2018-04-09 15:19:13 -0400

    more thoughts on conditional mutate with time incrementing.

--- data_cleaning/merge_in_adjusted_unemp.R
+++ data_cleaning/merge_in_adjusted_unemp.R
@@ -15,10 +15,25 @@ unemp_adju <- unemp_adju %>%
   mutate(Year = lubridate::year(date),
          Month = lubridate::month(date))
 
+#### Attempt 1: full join then filter ####
 # merge unemp_adju into events by date, series.
 # but ALSO keep unemp up to 6+ months after the event date
 test <- full_join(events, unemp_adju, by=c("Year", "Month", "series.id")) 
 test <- test %>% filter(date <= floor_date(EVENTS.begin_date, "month") & 
                           date <= floor_date(EVENTS.begin_date, "month") %m+% months(6))
 
+# this yields exactly the same number of obs as before, which i think is not right...
 
+#### mutate_if ####
+
+test <- events
+test <- test %>%
+  mutate(unemp_event_date = 
+           # condition (match on series.id/location, year and month)
+           if_else(unemp_adju$series.id == events$series.id & 
+                                      unemp_adju$Year == events$Year &
+                                      unemp_adju$Month == events$Month, 
+                   # TRUE
+                   unemp_adju$unemp,
+                   # FALSE
+                   NA))
