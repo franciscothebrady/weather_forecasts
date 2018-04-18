55d7c850cc2da8a235e864603234ce27a7d6d209
Francisco <franciscothebrady@gmail.com>
2018-04-18 17:30:05 -0400

    futzing around with unemployment columns.

--- data_cleaning/merge_in_adjusted_unemp.R
+++ data_cleaning/merge_in_adjusted_unemp.R
@@ -18,12 +18,32 @@ unemp_adju <- unemp_adju %>%
 #### Attempt 1: full join then filter ####
 # merge unemp_adju into events by date, series.
 # but ALSO keep unemp up to 6+ months after the event date
-test <- full_join(events, unemp_adju, by=c("Year", "Month", "series.id")) 
+test_full <- full_join(events, unemp_adju, by=c("series.id")) 
 test <- test %>% filter(date <= floor_date(EVENTS.begin_date, "month") & 
                           date <= floor_date(EVENTS.begin_date, "month") %m+% months(6))
 
 # this yields exactly the same number of obs as before, which i think is not right...
-
+test_full <- test_full %>% 
+  mutate(unemp_1 = ifelse(
+    add_with_rollback(test_full$EVENTS.begin_date, 
+                      months(1)) == test_full$date, test_full$unemp, NA),
+    unemp_2 = ifelse(
+      add_with_rollback(test_full$EVENTS.begin_date, 
+                        months(2)) == test_full$date, test_full$unemp, NA),
+    unemp_3 = ifelse(
+      add_with_rollback(test_full$EVENTS.begin_date, 
+                        months(3)) == test_full$date, test_full$unemp, NA),
+    unemp_4 = ifelse(
+      add_with_rollback(test_full$EVENTS.begin_date, 
+                        months(4)) == test_full$date, test_full$unemp, NA),
+    unemp_5 = ifelse(
+      add_with_rollback(test_full$EVENTS.begin_date, 
+                        months(5)) == test_full$date, test_full$unemp, NA),
+    unemp_6 = ifelse(
+      add_with_rollback(test_full$EVENTS.begin_date, 
+                        months(6)) == test_full$date, test_full$unemp, NA)
+    )
+test_full <- test_full %M% mutate(yearmon = zoo::yearmon())
 #### mutate & ifelse ####
 
 test <- events
